c3da8df2a83e7cd050db635b430892a3605060bb
Kamil Wójcik <van_kam@wp.pl>
2016-06-06 21:57:17 +0200

    Initial Commit

---
+++ przepizy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  przepizy
+//
+//  Created by Kamil Wójcik on 06.06.2016.
+//  Copyright © 2016 Kamil Wójcik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
